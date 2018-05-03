1b21379013bb558c01e3ec530be2db3b73e9d62f
wangjunchao-hj@huajiao.tv <wangjunchao-hj@huajiao.tv>
2018-05-03 09:34:53 +0800

    Initial Commit

---
+++ JCPopupContainerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCPopupContainerViewDemo
+//
+//  Created by huajiao on 2018/5/3.
+//  Copyright © 2018年 huajiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JCPopupContainerViewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  JCPopupContainerViewDemo
+//
+//  Created by huajiao on 2018/5/3.
+//  Copyright © 2018年 huajiao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
