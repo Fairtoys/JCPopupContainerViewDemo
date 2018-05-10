6db3399467fda06179403d615f97c7d215346f7e
Cerko <313574889@qq.com>
2018-05-10 22:08:09 +0800

    添加header

---
+++ JCPopupContainerViewDemo/JCViewOrientationAndScreenSizeAdaptionKit/JCViewOrientationAndScreenSizeAdaptionKit.h
@@ -0,0 +1,22 @@
+//
+//  JCViewOrientationAndScreenSizeAdaptionKit.h
+//  JCPopupContainerViewDemo
+//
+//  Created by huajiao on 2018/5/10.
+//  Copyright © 2018年 huajiao. All rights reserved.
+//
+
+#ifndef JCViewOrientationAndScreenSizeAdaptionKit_h
+#define JCViewOrientationAndScreenSizeAdaptionKit_h
+
+#import "UIDevice+JCOrientationAndScreenSizeUtils.h"
+
+#import "JCViewMultipleLayout.h"
+#import "JCViewMultipleLayout+JCOrientationAndScreenSizeSupport.h"
+
+#import "JCPopupUtils.h"
+#import "JCPopupUtils+JCOrientationAndScreenSizeSupport.h"
+#import "JCPopupUtilsLayoutAndAnimation.h"
+
+#import "JCPopupUtilsLayoutAndAnimations.h"
+#endif /* JCViewOrientationAndScreenSizeAdaptionKit_h */
